b450468131f406121625e23eab0ee6c8450aeb1d
ShreyDharmik <shreydharmik22@gmail.com>
2024-06-13 16:30:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-var typed= new typed(".text",{
-    String:["Frontend Developer", "Web Developer"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-})
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+  };
+  
+  ScrollReveal().reveal(".header__image img", {
+    ...scrollRevealOption,
+    origin: "right",
+  });
+  ScrollReveal().reveal(".header__content h1", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+  ScrollReveal().reveal(".header__content p", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+  ScrollReveal().reveal(".header__content form", {
+    ...scrollRevealOption,
+    delay: 1500,
+  });
